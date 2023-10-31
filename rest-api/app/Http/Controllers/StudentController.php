978ad1088fe591e78dceb0679a1cf96493e4f9c3
Saekyo <reskijunaedisalat1@gmail.com>
2023-11-01 03:03:49 +0700

    modifed migration & add try catch

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -4,36 +4,79 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use Illuminate\Support\Facades\Validator;
 class StudentController extends Controller
 {
     public function index()
 	{
-		$students = Student::all();
+        try {
+            $students = Student::all();
 
-		if (!empty($students)) {
-			$response = [
-				'message' => 'Menampilkan Data Semua Siswa',
-				'data' => $students,
-			];
-			return response()->json($response, 200);
-		} else {
-			$response = [
-				'message' => 'Data tidak ada'
-			];
-			return response()->json($response, 200);
-		}
+            if (count($students) > 0) {
+                $response = [
+                    'status' => HttpResponse::HTTP_OK,
+                    'message' => 'Menampilkan Data Semua Siswa',
+                    'data' => $students,
+                ];
+                return response()->json($response, HttpResponse::HTTP_OK);
+            } else {
+                $response = [
+                    'status' => HttpResponse::HTTP_OK,
+                    'message' => 'Data tidak ada'
+                ];
+                return response()->json($response, HttpResponse::HTTP_OK);
+            }
+        } catch (\Throwable $th) {
+            $response = [
+                'status' => HttpResponse::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => 'Internal Server Error'
+            ];
+            return response()->json($response, HttpResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
 	}
 
 	public function store(Request $request)
 	{
+        try {
+            $this->validate($request, [
+                'nama' => 'required',
+                'nim' => 'required',
+                'email' => 'required|email',
+                'jurusan' => 'required',
+            ]);
+            $student = Student::create($request->all());
 
-		$student = Student::create($request->all());
+            if($student) {
+                $response = [
+                    'status' => HttpResponse::HTTP_CREATED,
+                    'message' => 'Data Siswa Berhasil Dibuat',
+                    'data' => $student,
+                ];
 
-		$response = [
-			'message' => 'Data Siswa Berhasil Dibuat',
-			'data' => $student,
-		];
+                return response()->json($response, HttpResponse::HTTP_CREATED);
+            } else {
+                $response = [
+                    'status' => HttpResponse::HTTP_BAD_REQUEST,
+                    'message' => 'Gagal Menambahkan Data Siswa'
+                ];
+                return response()->json($response, HttpResponse::HTTP_BAD_REQUEST);
+            }
 
-		return response()->json($response, 201);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            $response = [
+                'status' => HttpResponse::HTTP_BAD_REQUEST,
+                'message' => 'Data yang dimasukkan tidak valid',
+                'errors' => $e->errors()
+            ];
+            return response()->json($response, HttpResponse::HTTP_BAD_REQUEST);
+
+        } catch (\Throwable $th) {
+            $response = [
+                'status' => HttpResponse::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => 'Internal Server Error'
+            ];
+            return response()->json($response, HttpResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
 	}
 }
--- rest-api/database/migrations/2023_10_31_191238_create_students_table.php
+++ rest-api/database/migrations/2023_10_31_191238_create_students_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('nim');
-            $table->string('email');
+            $table->string('nim')->unique();
+            $table->string('email')->unique();
             $table->string('jurusan');
             $table->timestamps();
         });
